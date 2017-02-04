e9af670df6724d2f5779e4afdef256bfbf734dde
carewb <amyeric@yeah.net>
2017-02-04 21:57:01 +0800

    get mp3 from iget

--- iget.py
+++ iget.py
@@ -1,23 +1,24 @@
 #coding=utf-8
-import sys
-import urllib.request
-import re
-import time
-import os
-import io 
 
+import sys
+import io
+import re
+import os
+import urllib.request
+
+log = os.path.split(os.path.realpath(sys.argv[0]))[0] + '\\iget.txt'
 topic_url_prefix="https://m.igetget.com/share/audio/aid/"
 url_fmt= topic_url_prefix + "%d"
-
-
+file = None
 
 def analysisOnePage(url):
-    print(url)
     content = getPageContent(url)
-    name, url = get_mp3_url(content)
-    if url!='':
-        download(url, name)
-
+    name, mp3_url = get_mp3_url(content)
+    if mp3_url != '':
+        #print(name, url)
+        writeToFile(name, url)
+        #download(mp3_url, name)
+        
 def getPageContent(url):
     try:
         wp = urllib.request.urlopen(url)
@@ -27,7 +28,8 @@ def getPageContent(url):
     except Exception as e:
         print(e)
         return getPageContent(url)
-        
+            
+            
 def get_mp3_url(content):
     title = ''
     url = ''
@@ -46,10 +48,15 @@ def get_mp3_url(content):
             url = res[0]
         
     return title, url.replace('&amp;', '&')
-
+    
 def download(url, name):
     urllib.request.urlretrieve(url, name+'.mp3')  
+    
+def writeToFile(title, url):
+    file.write(title + ':' + url + '\n')
 
 if __name__ == '__main__':
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')     
-    [analysisOnePage(url_fmt % i) for i in range(5000,7000)]
+    file = open(log, 'a+')
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
+    [analysisOnePage(url_fmt % i) for i in range(6500,7000)]
+    file.close()
