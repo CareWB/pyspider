fb3e5722cb1bc51a48dd198cad5a3f44e4fb366f
carewb <amyeric@yeah.net>
2017-02-14 22:27:10 +0800

    optimize the code

--- jiedu.py
+++ jiedu.py
@@ -8,23 +8,23 @@ import os
 import io
 import http.cookiejar  
 
-topic_url_prefix="http://www.jieduclub.com/"
-url_fmt= topic_url_prefix + "forum-57-%d.html"
+web_url="http://cmtv.cm/"
+url_fmt= web_url + "forum-57-%d.html"
 
 
 
 def analysisOnePage(url):
     content = getPageContent(url)
     topics = getTopics(content)
-    print(url)
-    print(len(topics))
-    print(topics)
+    #print(url)
+    #print(len(topics))
+    #print(topics)
 
     for topic in topics:
         url= getInfosFromTopic(topic)
-        print(url)
         thread_content = getPageContent(url)
         url_pre, name = get_mp3_url(thread_content)
+        #print(url_pre)
         download(url_pre, name)
 
 def getPageContent(url):
@@ -52,7 +52,7 @@ def getTopics(content):
 def getInfosFromTopic(topic):
     pattern = re.compile('''<a href="(thread-\d*-\d*-\d*.html)" onclick="atarget.*>.*</a>''',re.I)
     res = pattern.search(topic).groups()
-    return topic_url_prefix + res[0]
+    return web_url + res[0]
     
 def hasKeyString(title):
     for key in keys:
@@ -70,11 +70,11 @@ def webOpen(url):
     
 def login(hosturl, posturl):
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',  
-           'Referer' : 'http://www.jieduclub.com/forum.php'}  
+           'Referer' : web_url+'forum.php'}  
     postData = {'fastloginfield' : 'username',  
             'cookietime':'2592000',
-            'username' : 'xxxxxx',
-            'password' : 'xxxxxx', #你的密码，密码可能是明文传输也可能是密文，如果是密文需要调用相应的加密算法加密  
+            'username' : 'xxx',
+            'password' : 'xxx',
             }  
     cj = http.cookiejar.LWPCookieJar()  
     cookie_support = urllib.request.HTTPCookieProcessor(cj)  
@@ -101,11 +101,11 @@ def get_mp3_url(content):
 
 if __name__ == '__main__':
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
-    hosturl = 'http://www.jieduclub.com/forum.php'
-    posturl = 'http://www.jieduclub.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
+    hosturl = web_url + 'forum.php'
+    posturl = web_url + 'member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
     login(hosturl, posturl)
     
-    [analysisOnePage(url_fmt % i) for i in range(1,7)]
+    [analysisOnePage(url_fmt % i) for i in range(7,8)]
     
     #analysisOnePage('http://www.jieduclub.com/thread-2421-1-1.html')
     #file.close()
