c2f0202cd768bd183d156465fa1ca26b18013af3
carewb <amyeric@yeah.net>
2017-03-04 01:16:23 +0800

    update

--- baidulvyou.py
+++ baidulvyou.py
@@ -6,35 +6,20 @@ import webbrowser
 import time
 import os
 import io
-import http.cookiejar  
+import json 
 
-site_url="http://www.mafengwo.cn/"
+web_url="http://lvyou.baidu.com/destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl="
+url_fmt= web_url + "%s&pn=%d&rn=%d"
 log = os.path.split(os.path.realpath(sys.argv[0]))[0] + os.sep + 'baidulvyou.txt'
 
 file = None
 
-def get_city_page(city):
-    return get_page_content(site_url + city)
-    
-    
-def get_all_plan(content):
-    #<a class="plan-title" href="/plan/38137129977e53e2d69d9a8b" target="_blank">厦门5日游</a>
-    pattern = re.compile('''<a class="plan-title" href="(.*)" target="_blank">(.*)</a>''',re.M)
-    all = re.findall(pattern, content)
-    [get_one_kind_plan(site_url + day_plan[0])]
-    #for day_plan in all:
-    #    file.write('{1}:{0}\n'.format(day_plan[0], day_plan[1]).encode())
-    
-def get_one_kind_plan(one_kind_url):
-    content = get_page_content(one_kind_url)
-    
-    
 
 def get_page_content(url):
     try:
         wp = urllib.request.urlopen(url)
         all = wp.read()
-        content = str(all.decode('utf-8','ignore'))
+        content = str(all.decode("utf8"))
         time.sleep(1)
         return content
     except Exception as e:
@@ -42,34 +27,25 @@ def get_page_content(url):
         time.sleep(1)
         return page_content(url)
 
-def get_one_page_scenics(hosturl, posturl, i):
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',  
-           'Referer' : 'http://www.mafengwo.cn/jd/10132/gonglve.html'}  
-    postData = {'sAct' : 'KMdd_StructWebAjax|GetPoisByTag',  
-            'iMddid':'10132',
-            'iTagId' : '0',
-            'iPage' : str(i),
-            }  
-    cj = http.cookiejar.LWPCookieJar()  
-    cookie_support = urllib.request.HTTPCookieProcessor(cj)  
-    opener = urllib.request.build_opener(cookie_support, urllib.request.HTTPHandler)  
-    urllib.request.install_opener(opener)  
-    h = urllib.request.urlopen(hosturl)  
-    postData = urllib.parse.urlencode(postData).encode('utf-8')  
-  
-    request = urllib.request.Request(posturl, postData, headers)  
-    response = urllib.request.urlopen(request)
-    pattern = re.compile('''<a href=".*poi.*/(.*)" target="_blank" title="(.*)">''',re.M)
-    return re.findall(pattern, response.read().decode("unicode_escape"))
-    #file.write(text.decode("unicode_escape").encode())
-    #print(text.decode("unicode_escape"))
+def get_scenic_info(url):
+    jsonData = json.loads(get_page_content(url), strict=False)
+    for data in jsonData["data"]["scene_list"]:
+        price = '0'
+        if data['ticket'] is not None:
+            price = data['ticket']['price']
+        print('{0}:{6}map-{1},{2},${3},{4},{5}'.format( \
+            str(data['sname']), \
+            str(data['ext']['map_info']), \
+            str(data['ext']['avg_remark_score']), \
+            str(price), \
+            str(data['ext']['address']), \
+            str(data['ext']['abs_desc']), \
+            str(data['surl'])))
+    return jsonData["data"]["scene_list"][0]['sname'].encode()
+
 
 if __name__ == '__main__':
     #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
     file = open(log, 'wb')
-    #get_all_plan(get_city_page('xiamen'))
-    all = get_one_page_scenics('http://www.mafengwo.cn','http://www.mafengwo.cn/ajax/router.php', 1)
-    print(all[0][1])
-    content = get_page_content(site_url + 'poi/' + all[0][0])
-    file.write(content.encode())
+    [file.write(get_scenic_info(url_fmt % ('xiamen', i, 30))) for i in range(1,2)]
     file.close()
