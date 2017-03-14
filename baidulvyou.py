84eb997d7c9792a8f380392576e89ff9d14cc429
carewb <amyeric@yeah.net>
2017-03-14 23:19:35 +0800

    baidulvyou add some fileds

--- baidulvyou.py
+++ baidulvyou.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 import sys
 import urllib.request
 import re
@@ -6,11 +6,13 @@ import webbrowser
 import time
 import os
 import io
-import json 
+import json
 
-web_url="http://lvyou.baidu.com/destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl="
-url_fmt= web_url + "%s&pn=%d&rn=%d"
-log = os.path.split(os.path.realpath(sys.argv[0]))[0] + os.sep + 'baidulvyou.txt'
+web_site = "http://lvyou.baidu.com/"
+web_url = web_site + "destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl="
+url_fmt = web_url + "%s&pn=%d&rn=%d"
+log = os.path.split(os.path.realpath(sys.argv[0]))[
+    0] + os.sep + 'baidulvyou.txt'
 
 file = None
 
@@ -27,25 +29,58 @@ def get_page_content(url):
         time.sleep(1)
         return page_content(url)
 
-def get_scenic_info(url):
+
+def get_scenics_info(url):
+    scenics = {}
     jsonData = json.loads(get_page_content(url), strict=False)
     for data in jsonData["data"]["scene_list"]:
+        info = {}
         price = '0'
         if data['ticket'] is not None:
             price = data['ticket']['price']
-        print('{0}:{6}map-{1},{2},${3},{4},{5}'.format( \
-            str(data['sname']), \
-            str(data['ext']['map_info']), \
-            str(data['ext']['avg_remark_score']), \
-            str(price), \
-            str(data['ext']['address']), \
-            str(data['ext']['abs_desc']), \
-            str(data['surl'])))
-    return jsonData["data"]["scene_list"][0]['sname'].encode()
+
+        info['name'] = str(data['sname'])
+        info['lng'], info['lat'] = str(data['ext']['map_info']).split(',', 1)
+        info['score'] = str(data['ext']['avg_remark_score'])
+        info['price'] = str(price)
+        info['address'] = str(data['ext']['address'])
+        info['desc'] = str(data['ext']['abs_desc'])
+        info['surl'] = str(data['surl'])
+
+        scenics[info['name']] = info
+
+    for k, v in scenics.items():
+        get_scenic_detail(v)
+
+    return scenics
+
+
+def get_scenic_detail(scenic):
+    content = get_page_content(web_site + scenic['surl'])
+
+    pattern = re.compile('''景点类型：(.*)''', re.I)
+    ma = pattern.search(content)
+    if ma is not None:
+        res = ma.groups()
+        scenic['type'] = res[0]
+
+    pattern = re.compile('''建议游玩：(.*)''', re.I)
+    ma = pattern.search(content)
+    if ma is not None:
+        res = ma.groups()
+        scenic['play_time'] = res[0]
+
+    return scenic
+
+
+def write_to_file(scenics):
+    print(scenics)
+    
 
 
 if __name__ == '__main__':
-    #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') 
+    #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
     file = open(log, 'wb')
-    [file.write(get_scenic_info(url_fmt % ('xiamen', i, 30))) for i in range(1,2)]
+    [write_to_file(get_scenics_info(url_fmt % ('xiamen', i, 30)))
+     for i in range(1, 3)]
     file.close()
