acd51aa155dc454e9ce6f38ed053851c372cc0db
carewb <amyeric@yeah.net>
2017-03-20 18:55:55 +0800

    get hotels

--- baidulvyou.py
+++ baidulvyou.py
@@ -7,6 +7,8 @@ import time
 import os
 import io
 import json
+import pymysql
+
 
 web_site = "http://lvyou.baidu.com/"
 web_url = web_site + "destination/ajax/jingdian?format=ajax&cid=0&playid=0&seasonid=5&surl="
@@ -15,6 +17,7 @@ log = os.path.split(os.path.realpath(sys.argv[0]))[
     0] + os.sep + 'baidulvyou.txt'
 
 file = None
+hotels = {}
 
 
 def get_page_content(url):
@@ -73,14 +76,93 @@ def get_scenic_detail(scenic):
     return scenic
 
 
-def write_to_file(scenics):
-    print(scenics)
-    
+def get_hotels_json_data(content_lines):
+    amount_json = None
+    detail_json = None
+    amount_found = False
+    detail_fonud = False
+
+    for line in content_lines:
+        if line.find('htllist: ') > 0:
+            amount_found = True
+            line = line[line.find('['): line.find(']') + 1]
+            amount_json = json.loads(
+                '{"data":' + line.strip().rstrip(',') + '}', strict=False)
+        elif line.find('hotelPositionJSON: ') > 0:
+            amount_found = True
+            line = line[line.find('['): line.find(']') + 1]
+            detail_json = json.loads(
+                '{"data":' + line.strip().rstrip(',') + '}', strict=False)
+
+        if amount_found and detail_fonud:
+            break
+
+    return amount_json, detail_json
+
+
+def get_one_page_hotels(page_url):
+    content = get_page_content(page_url)
+    lines = io.StringIO(content).readlines()
+    amount_json, detail_json = get_hotels_json_data(lines)
+
+    if amount_json is not None:
+        for data in amount_json['data']:
+            hotels[data['hotelid']] = {}
+            hotels[data['hotelid']]['id'] = data['hotelid']
+            hotels[data['hotelid']]['amount'] = data['amount']
+
+    if detail_json is not None:
+        for data in detail_json['data']:
+            hotels[data['id']]['name'] = data['name']
+            hotels[data['id']]['lat'] = data['lat']
+            hotels[data['id']]['lon'] = data['lon']
+            hotels[data['id']]['url'] = data['url']
+            hotels[data['id']]['address'] = data['address']
+            hotels[data['id']]['score'] = data['score']
+
+    insert_into_database(hotels)
+
+
+def insert_into_database(hotels):
+    conn = pymysql.connect(host='', port=3306, user='',
+                           passwd='', db='', use_unicode=True, charset="utf8")
+    cursor = conn.cursor()
+    sql = """DELETE FROM IMHotel"""
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except:
+        conn.rollback()
+
+    sql = 'INSERT INTO `IMHotel` VALUES '
+
+    for k, v in hotels.items():
+        sql += """({id},'{city}','{name}',{score},'{tags}',{must},'{url}','{class_}',{price},{distance},0,'{lng}','{lat}'),""".format(
+            id=k, city='XMN', name=v['name'], score=v['score'], tags='经济型', must=1, url=v['url'],
+            class_=1, price=v['amount'], distance=100, lng=v['lon'], lat=v['lat'])
+    sql = sql[:-1] + ';'
+    print(sql)
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        conn.rollback()
+
+    cursor.close()
+    conn.close()
+
+
+def write_to_file(content):
+    for k, v in content.items():
+        print(v)
 
 
 if __name__ == '__main__':
     #sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
     file = open(log, 'wb')
-    [write_to_file(get_scenics_info(url_fmt % ('xiamen', i, 30)))
-     for i in range(1, 3)]
+    #[write_to_file(get_scenics_info(url_fmt % ('xiamen', i, 30)))
+    # for i in range(1, 3)]
+
+    get_one_page_hotels('http://hotels.ctrip.com/hotel/xiamen25&p1')
     file.close()
